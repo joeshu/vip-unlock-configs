5efad00890a64a5503ecdc98e7d634f26bf71599
joeshu <350027830@qq.com>
2026-03-21 10:20:25 +0800

    Update Unified_VIP_Unlock_Manager.js

--- Unified_VIP_Unlock_Manager.js
+++ Unified_VIP_Unlock_Manager.js
@@ -53,7 +53,7 @@
 const CONFIG = {
   REMOTE_BASE: 'https://joeshu.github.io/vip-unlock-configs',
   //CACHE_TTL: 1 * 20 * 60 * 1000,
-  CACHE_TTL: 1 * 2 * 60* 1000,
+  CACHE_TTL: 6 * 60 * 60* 1000,
   DEBUG: true,
   TIMEOUT: 10
 };
